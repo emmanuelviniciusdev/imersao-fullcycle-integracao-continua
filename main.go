d6dc7481abed89853c879950eea4f5b77f57b8f9
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2024-02-12 10:41:59 -0300

    SonarQube configs

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	result := Sum(1, 1)
-	fmt.Println("The result of 1 + 1 is:", result)
+	fmt.Println(Sum(1, 1))
+	fmt.Println(Subtract(1, 1))
+	fmt.Println(Multiply(1, 1))
+	fmt.Println(Divide(1, 1))
+}
+
+func Divide(x float64, y float64) float64 {
+	return x / y
+}
+
+func Multiply(x float64, y float64) float64 {
+	return x * y
+}
+
+func Subtract(x float64, y float64) float64 {
+	return x - y
 }
 
 func Sum(x float64, y float64) float64 {
+	if x == 10 {
+		return 10
+	} else if x == 20 {
+		return 20
+	}
+
 	return x + y
 }
